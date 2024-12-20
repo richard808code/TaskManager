721736542cdc12a180b0c4e909721f7f29fc6db5
richard808code <richard.stryk@icloud.com>
2024-12-20 21:03:14 +0100

    Added user input handling and some color coding

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -9,18 +9,18 @@ import java.util.Scanner;
 
 public class TaskManager {
     public static void main(String[] args) {
-        userOptions();
+        Scanner scanner = new Scanner(System.in);
         String[][] tasks = readFile();
-        removeTask(tasks);
-
+        handleUserInput(tasks, scanner);
     }
 
     public static void userOptions() {
-        System.out.println(ConsoleColors.BLUE + "Please select an option:" + ConsoleColors.RESET);
+        System.out.println(ConsoleColors.BLUE_BOLD_BRIGHT + "Please select an option from the following:" + ConsoleColors.RESET);
         String[] options = {"add", "remove", "list", "exit"};
         for (String option : options) {
-            System.out.println(option);
+            System.out.println(ConsoleColors.WHITE_BRIGHT + option + ConsoleColors.RESET);
         }
+        System.out.println(ConsoleColors.BLUE_BRIGHT + "Type in the selected option:" + ConsoleColors.RESET);
     }
 
     public static String[][] readFile() {
@@ -57,38 +57,48 @@ public class TaskManager {
         return tasks;
     }
 
-//    public static void handleUserInput(){
-//        final var scanner = new Scanner(System.in);
-//        String input = scanner.nextLine();
-//
-//        switch (input) {
-//            case "add":
-//                addTask();
-//                break;
-//            case "remove":
-//                removeTask();
-//                break;
-//            case "list":
-//                listTasks();
-//                break;
-//            case "exit":
-//                exitTasks();
-//                break;
-//            default:
-//                System.out.println("Please select a correct option.");
-//        }
-//    }
+    public static void handleUserInput(String[][] tasks, Scanner scanner) {
+        String input;
+        do {
+            userOptions();
+            input = scanner.nextLine();
 
-    public static String[][] addTask(String[][] tasks) {
+            switch (input) {
+                case "add":
+                    tasks = addTask(tasks, scanner);
+                    break;
+                case "remove":
+                    tasks = removeTask(tasks, scanner);
+                    break;
+                case "list":
+                    listTasks(tasks);
+                    break;
+                case "exit":
+                    exitTasks(tasks, scanner);
+                    break;
+                default:
+                    System.err.println("Please select a correct option!");
+            }
+        } while (!input.equalsIgnoreCase("exit"));
+    }
 
-        final var scanner = new Scanner(System.in);
-        System.out.println(ConsoleColors.GREEN + "Please enter the description of the task including importance: " + ConsoleColors.RESET);
+    public static String[][] addTask(String[][] tasks, Scanner scanner) {
+
+        System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "Please enter the description of the task including importance: " + ConsoleColors.RESET);
         String task = scanner.nextLine();
-        System.out.println(ConsoleColors.GREEN + "Please enter the deadline(YYYY-MM-DD): " + ConsoleColors.RESET);
+        System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "Please enter the deadline(YYYY-MM-DD): " + ConsoleColors.RESET);
         String deadline = scanner.nextLine();
-        System.out.println(ConsoleColors.GREEN + "Please enter true if the task is done or enter false if it is yet to be done: " + ConsoleColors.RESET);
-        Boolean done = scanner.nextBoolean();
-        scanner.close();
+        Boolean done = null;
+        while (done == null) {
+            System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "Please enter true if the task is done or enter false if it is yet to be done: " + ConsoleColors.RESET);
+            if (scanner.hasNextBoolean()) {
+                done = scanner.nextBoolean();
+                scanner.nextLine();
+            } else {
+                System.err.println("Invalid input. Please enter true or false.");
+                scanner.next();
+            }
+        }
 
         tasks = Arrays.copyOf(tasks, tasks.length + 1);
         tasks[tasks.length - 1] = new String[3];
@@ -96,6 +106,8 @@ public class TaskManager {
         tasks[tasks.length - 1][0] = task;
         tasks[tasks.length - 1][1] = deadline;
         tasks[tasks.length - 1][2] = String.valueOf(done);
+
+        System.out.println(ConsoleColors.GREEN + "Task added successfully!" + ConsoleColors.RESET);
 
         return tasks;
     }
@@ -106,34 +118,37 @@ public class TaskManager {
         }
         for (int i = 0; i < tasks.length; i++) {
             System.out.println(ConsoleColors.BLUE_BOLD + "Task #" + (i + 1) + ": " + ConsoleColors.RESET
-                    + tasks[i][0] + ConsoleColors.BLUE + " Deadline: " + ConsoleColors.RESET
-                    + tasks[i][1] + ConsoleColors.BLUE + " Completion: " + ConsoleColors.RESET
+                    + tasks[i][0] + ConsoleColors.YELLOW + " Deadline: " + ConsoleColors.RESET
+                    + tasks[i][1] + ConsoleColors.PURPLE + " Completion: " + ConsoleColors.RESET
                     + tasks[i][2] + "\n");
         }
     }
 
-    public static String[][] removeTask(String[][] tasks) {
+    public static String[][] removeTask(String[][] tasks, Scanner scanner) {
         listTasks(tasks);
-        System.out.println(ConsoleColors.GREEN + "Insert the number of the task to remove: " + ConsoleColors.RESET);
-        final var scanner = new Scanner(System.in);
-        while (scanner.hasNextInt()) {
-            int taskIndex = scanner.nextInt();
-            if (taskIndex <= 0 || taskIndex > tasks.length) {
-                System.err.println("ERROR: Invalid task index!");
-                System.out.println("Insert valid task index:");
+        System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "Insert the number of the task to remove: " + ConsoleColors.RESET);
+
+        while (true) {
+            if (scanner.hasNextInt()) {
+                int taskIndex = scanner.nextInt();
+                scanner.nextLine();
+                if (taskIndex > 0 && taskIndex <= tasks.length) {
+                    tasks = ArrayUtils.remove(tasks, taskIndex - 1);
+                    System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "Task removed successfully!" + ConsoleColors.RESET);
+                    break;
+                } else {
+                    System.err.println("Invalid index, try again.");
+                }
             } else {
-                System.out.println("Array length before removal: " + tasks.length);
-                tasks = ArrayUtils.remove(tasks, taskIndex - 1);
-                System.out.println("Array length after removal: " + tasks.length);
-                System.out.println(ConsoleColors.GREEN + "Task removed successfully!");
-                break;
+                System.err.println("Please enter a number!");
+                scanner.next();
             }
         }
 
         return tasks;
     }
 
-    public static void exitTasks(String[][] tasks) {
+    public static void exitTasks(String[][] tasks, Scanner scanner) {
         final var path = Paths.get("src/main/resources/tasks.csv");
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(path.toFile()))) {
@@ -146,6 +161,7 @@ public class TaskManager {
             e.printStackTrace();
         }
         System.out.println(ConsoleColors.RED + "The program finished!" + ConsoleColors.RESET);
+        scanner.close();
         System.exit(0);
     }
 }
