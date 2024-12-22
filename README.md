ed832d0652c8502c9974f4b7dd138ca93af81066
richard808code <richard.stryk@icloud.com>
2024-12-22 09:21:07 +0100

    Cleaned up the code according to suggestions; Updated the readme file

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # TaskManager
 **Simple Task Manager program for the Coderslab Java Workshop**
-## Functions
+## Basic Functions
 1.Adding tasks
 
 2.Removing tasks
 
 3.Viewing the list of tasks
+
+## Version 1.1
+### 2024-12-22
+Cleaned up the code according to suggestions, now it should be more readable and simpler.
+
+## How does the program work?
+This is a simple console program that reads information about tasks from a .csv file, can modify them
+and afterward save the back into the file.
+
+
--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -3,23 +3,44 @@ package pl.coderslab;
 import org.apache.commons.lang3.ArrayUtils;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class TaskManager {
 
-    // CLEAN-CODE: promenne tasks a scanner bych dal jako globalni promenne pristupne ze vsech metod
-    //    private static String[][] tasks;
-    //    private static Scanner scanner;
+    private static String[][] tasks;
+    private static Scanner scanner;
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String[][] tasks = readFile(); // CLEAN-CODE: prosim prejmenuj metodu readFile() na vice popisnou readTasks()
-        handleUserInput(tasks, scanner);
+        scanner = new Scanner(System.in);
+        tasks = readTasks();
+        String input;
+        do {
+            displayUserOptions();
+            input = scanner.nextLine().toLowerCase();
+
+            switch (input) {
+                case "add":
+                    tasks = addTask();
+                    break;
+                case "remove":
+                    tasks = removeTask();
+                    break;
+                case "list":
+                    listTasks();
+                    break;
+                case "exit":
+                    exitTasks();
+                    break;
+                default:
+                    System.err.println("Please select a correct option!");
+            }
+        } while (!input.equalsIgnoreCase("exit"));
     }
 
-    public static void userOptions() {
+    public static void displayUserOptions() {
         System.out.println(ConsoleColors.BLUE_BOLD_BRIGHT + "Please select an option from the following:" + ConsoleColors.RESET);
         String[] options = {"add", "remove", "list", "exit"};
         for (String option : options) {
@@ -28,90 +49,28 @@ public class TaskManager {
         System.out.println(ConsoleColors.BLUE_BRIGHT + "Type in the selected option:" + ConsoleColors.RESET);
     }
 
-    public static String[][] readFile() {
-        /*
-        CLEAN-CODE: jednodussi verze na mene radku
+    public static String[][] readTasks() {
+        final var path = Paths.get("src/main/resources/tasks.csv");
         try {
-            final var lines = Files.readAllLines(TASK_FILE_PATH);
+            final var lines = Files.readAllLines(path);
 
             tasks = new String[lines.size()][];
             for (int i = 0; i < lines.size(); i++) {
-                tasks[i] = lines.get(i).split(", ");
+                tasks[i] = lines.get(i).split(",");
             }
         } catch (IOException e) {
             System.err.println("Failed to load tasks.csv");
             e.printStackTrace(System.err);
             System.exit(-1);
         }
-         */
-
-        final var path = Paths.get("src/main/resources/tasks.csv");
-        int lineCounter = 0;
-        String[][] tasks = null;
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(path.toFile()))) {
-            while (reader.readLine() != null) {
-                lineCounter++;
-            }
-
-            reader.close(); //BUG: reader uz je uzavreny v ramci try-with-resources bloku
-            try (BufferedReader newReader = new BufferedReader(new FileReader(path.toFile()))) {
-                String[] firstLine = newReader.readLine().split(",");
-                tasks = new String[lineCounter][firstLine.length];
-                tasks[0] = firstLine;
-
-                for (int i = 1; i < lineCounter; i++) {
-                    String line = newReader.readLine();
-                    if (line != null) {
-                        tasks[i] = line.split(",");
-                    }
-                }
-            }
-        } catch (FileNotFoundException e) {
-            System.err.println("ERROR: File not found!");
-            e.printStackTrace(System.err);
-            //BUG: zde bych program zastavil dalsi chod programu uz nema smysl System.exit(-1);
-        } catch (IOException e) {
-            System.err.println("ERROR: I/O error while reading file!");
-            //BUG: zde bych program zastavil dalsi chod programu uz nema smysl System.exit(-1);
-            e.printStackTrace(System.err);
-        }
-
         return tasks;
     }
 
-    public static void handleUserInput(String[][] tasks, Scanner scanner) {
-        // CLEAN-CODE: tuhle sekci bych celou predal do main metody aby bylo hned jasne co program dela
-        String input;
-        do {
-            userOptions(); // CLEAN-CODE: userOptions() -> displayUserOptions() hned je z nazvu metody jasne co metoda dela
-            input = scanner.nextLine(); // //IMPROVE: scanner.nextLine().toLowerCase(), umozni ti to zadat jak upper case tak lower case
-
-            switch (input) {
-                case "add":
-                    tasks = addTask(tasks, scanner); //CLEAN-CODE: jak bude promenna tasks a scanner globalni tak zde se zjednodussi a bude jen metoda addTask()
-                    break;
-                case "remove":
-                    tasks = removeTask(tasks, scanner); //CLEAN-CODE: jak bude promenna tasks a scanner globalni tak zde se zjednodussi a bude jen metoda removeTask()
-                    break;
-                case "list":
-                    listTasks(tasks); //CLEAN-CODE: jak bude promenna tasks a scanner globalni tak zde se zjednodussi a bude jen metoda listTasks()
-                    break;
-                case "exit":
-                    exitTasks(tasks, scanner); //CLEAN-CODE: jak bude promenna tasks a scanner globalni tak zde se zjednodussi a bude jen metoda exit(), asi lepsi pojmenovani nez exitTasks()
-                    break;
-                default:
-                    System.err.println("Please select a correct option!");
-            }
-        } while (!input.equalsIgnoreCase("exit"));
-    }
-
-    public static String[][] addTask(String[][] tasks, Scanner scanner) {
+    public static String[][] addTask() {
         System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "Please enter the description of the task including importance: " + ConsoleColors.RESET);
-        String task = scanner.nextLine(); //CLEAN-CODE: promenne v metode defaultne nastavujeme na final, znamena to ze promenna po vytvoreni je nemenna coz je zde chteny stav
-        // CLEAN-CODE: task -> description je to vice popisne a muzes pak pouzit jmeno task pro pole
+        final String description = scanner.nextLine();
         System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "Please enter the deadline(YYYY-MM-DD): " + ConsoleColors.RESET);
-        String deadline = scanner.nextLine(); //CLEAN-CODE: promenne v metode defaultne nastavujeme na final, znamena to ze promenna po vytvoreni je nemenna coz je zde chteny stav
+        final String deadline = scanner.nextLine();
         Boolean done = null;
         while (done == null) {
             System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "Please enter true if the task is done or enter false if it is yet to be done: " + ConsoleColors.RESET);
@@ -125,30 +84,19 @@ public class TaskManager {
         }
 
         tasks = Arrays.copyOf(tasks, tasks.length + 1);
-        tasks[tasks.length - 1] = new String[3];
-
-        tasks[tasks.length - 1][0] = task;
-        tasks[tasks.length - 1][1] = deadline;
-        tasks[tasks.length - 1][2] = String.valueOf(done);
-
-
-        /*
-        //CLEAN-CODE:
         final var task = new String[3];
-        task[0] = task;
+        task[0] = description;
         task[1] = deadline;
         task[2] = String.valueOf(done);
 
         tasks[tasks.length - 1] = task;
-        */
-
 
         System.out.println(ConsoleColors.GREEN + "Task added successfully!" + ConsoleColors.RESET);
 
         return tasks;
     }
 
-    public static void listTasks(String[][] tasks) {
+    public static void listTasks() {
         if (tasks.length == 0) {
             System.out.println(ConsoleColors.RED + "No tasks found!" + ConsoleColors.RESET);
         }
@@ -160,8 +108,8 @@ public class TaskManager {
         }
     }
 
-    public static String[][] removeTask(String[][] tasks, Scanner scanner) {
-        listTasks(tasks);
+    public static String[][] removeTask() {
+        listTasks();
         System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "Insert the number of the task to remove: " + ConsoleColors.RESET);
 
         while (true) {
@@ -184,9 +132,7 @@ public class TaskManager {
         return tasks;
     }
 
-    public static void exitTasks(String[][] tasks, Scanner scanner) {
-
-        //CLEAN-CODE: kod zapisujici seznam ukolu do souboru predat do metody saveTasksToFile()
+    public static void saveTasksToFile() {
         final var path = Paths.get("src/main/resources/tasks.csv");
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(path.toFile()))) {
@@ -198,6 +144,10 @@ public class TaskManager {
             System.err.println("ERROR: I/O error while writing file!");
             e.printStackTrace();
         }
+    }
+
+    public static void exitTasks() {
+        saveTasksToFile();
         System.out.println(ConsoleColors.RED + "The program finished!" + ConsoleColors.RESET);
         scanner.close();
         System.exit(0);
